6fb31ba81a8ac70ef9a4a720e8f9660babd7f79d
turnzz <pricetur000@hsestudents.org>
2024-05-17 12:33:35 -0400

    fixed Student.java.

--- src/main/java/com/csase/PlannerRunner.java
+++ src/main/java/com/csase/PlannerRunner.java
@@ -9,5 +9,8 @@ public class PlannerRunner {
         // Gets the student's choice until the student exits
         jada.getChoice();
 
+        Student jada2 = new Student();
+        jada2.getChoice();
+
     }
 }
